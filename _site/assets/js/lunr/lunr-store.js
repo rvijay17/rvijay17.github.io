fa7b8b80c54f7f44366437047b1e1f44d44bb4d7
Vijay Rajagopalan <rvijay17@gmail.com>
2019-03-05 19:06:05 +1100

    changed the bio photo

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "CitiBike Analysis",
         "excerpt":"CitiBike Analysis Part 1 In this post, I’m just trying the load, cleanse and analyse the CitiBike data for March 2016. In Part 2, I’ll import the weather data and see if that affects the bike usage pattern. The ETL part of the analysis is in a separate file (CitiBike_ETL.py)....","categories": [],
         "tags": ["open-source","etl"],
-        "url": "https://rvijay17.github.io/CitiBike_Analysis/",
+        "url": "/CitiBike_Analysis/",
         "teaser":null}]
