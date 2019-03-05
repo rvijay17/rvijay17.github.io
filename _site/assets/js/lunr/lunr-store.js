fdff15b75369e79353919fee3692ae3aeecd82ae
Vijay Rajagopalan <rvijay17@gmail.com>
2019-03-05 11:54:03 +1100

    Deleted unnecessary markdown posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,31 +1,6 @@
 var store = [{
-        "title": "What's Jekyll?",
-        "excerpt":"Jekyll is a static site generator, an open-source tool for creating simple yet powerful websites of all shapes and sizes. From the project’s readme: Jekyll is a simple, blog aware, static site generator. It takes a template directory […] and spits out a complete, static website suitable for serving with...","categories": [],
-        "tags": [],
-        "url": "/whats-jekyll-copy/",
-        "teaser":null},{
-        "title": "What's Jekyll?",
-        "excerpt":"Jekyll is a static site generator, an open-source tool for creating simple yet powerful websites of all shapes and sizes. From the project’s readme: Jekyll is a simple, blog aware, static site generator. It takes a template directory […] and spits out a complete, static website suitable for serving with...","categories": [],
-        "tags": [],
-        "url": "/whats-jekyll-copy-2/",
-        "teaser":null},{
-        "title": "What's Jekyll?",
-        "excerpt":"Jekyll is a static site generator, an open-source tool for creating simple yet powerful websites of all shapes and sizes. From the project’s readme: Jekyll is a simple, blog aware, static site generator. It takes a template directory […] and spits out a complete, static website suitable for serving with...","categories": [],
-        "tags": [],
-        "url": "/whats-jekyll/",
-        "teaser":null},{
-        "title": "Example content",
-        "excerpt":"Howdy! This is an example blog post that shows several types of HTML content supported in this theme. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur...","categories": [],
-        "tags": [],
-        "url": "/example-content/",
-        "teaser":null},{
-        "title": "Introducing Lanyon",
-        "excerpt":"Lanyon is an unassuming Jekyll theme that places content first by tucking away navigation in a hidden drawer. It’s based on Poole, the Jekyll butler. Built on Poole Poole is the Jekyll Butler, serving as an upstanding and effective foundation for Jekyll themes by @mdo. Poole, and every theme built...","categories": [],
-        "tags": [],
-        "url": "/introducing-lanyon/",
-        "teaser":null},{
         "title": "CitiBike Analysis",
         "excerpt":"CitiBike Analysis Part 1 In this post, I’m just trying the load, cleanse and analyse the CitiBike data for March 2016. In Part 2, I’ll import the weather data and see if that affects the bike usage pattern. The ETL part of the analysis is in a separate file (CitiBike_ETL.py)....","categories": [],
         "tags": ["open-source","etl"],
-        "url": "/CitiBike_Analysis/",
+        "url": "https://rvijay17.github.io/CitiBike_Analysis/",
         "teaser":null}]
